6f1eec81e1285ae1aa1cece0971b92a8cf2d3f7e
potzenhotz <lukasmuessle@gmail.com>
2019-10-08 14:04:20 +0200

    further functionallity

--- s3_bucket_testing.py
+++ s3_bucket_testing.py
@@ -25,10 +25,25 @@ def upload_to_aws(local_file, bucket, s3_file):
 		print("Credentials not available")
 		return False
 
-list_buckets
-uploaded = upload_to_aws('{}/test.csv'.format(dir_path), 'potzenhotz-python-test', 'test.csv')
-print(uploaded)
+def download_from_aws(local_file, bucket, s3_file):
+	s3 = boto3.client('s3')
+	try:
+		s3.download_file(bucket, s3_file, local_file)
+		print("Download Successful")
+		return True
+	except FileNotFoundError:
+		print("The file was not found")
+		return False
+	except NoCredentialsError:
+		print("Credentials not available")
+		return False
 
+list_buckets
+#uploaded = upload_to_aws('{}/test.csv'.format(dir_path), 'potzenhotz-python-test', 'test.csv')
+#print(uploaded)
+
+downloaded = download_from_aws('{}/test_2.csv'.format(dir_path), 'potzenhotz-python-test', 'test.csv')
+print(downloaded)
 
 
 
